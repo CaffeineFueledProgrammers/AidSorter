83bf4c50b855b154302c57a2d74db6926ccb9970
chris1320 <chris1320is@protonmail.com>
2024-06-12 12:51:20 +0800

    style(host): add docstrings
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ host/aidsorter/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+"""
+AidSorter - Automatic Goods Sorting System
+"""
--- host/aidsorter/__main__.py
+++ host/aidsorter/__main__.py
@@ -1,7 +1,17 @@
+"""
+AidSorter - Automatic Goods Sorting System
+"""
+
 import sys
 
 
 def main() -> int:
+    """
+    The main function of the program.
+
+    :returns int: The exit code of the program.
+    """
+
     print("AidSorter")
     return 0
 
