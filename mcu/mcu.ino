ee030c1434a2617b982e34794f08420834a0e969
chris1320 <chris1320is@protonmail.com>
2024-07-08 22:37:16 +0800

    feat(host): fix servo opening
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -22,10 +22,12 @@
 #define pin_ir_bucket5 6
 #define pin_error_led 7
 
-#define servo_angle_open 0   // Angle to open the servo
-#define servo_angle_close 90 // Angle to close the servo
+#define servo_angle_open_platform 0  // Angle to open the servo of the platform
+#define servo_angle_open12 35        // Angle to open the servo 1 & 2
+#define servo_angle_open34 155       // Angle to open the servos 3 & 4
+#define servo_angle_close 90         // Angle to close the servo
 
-#define debounce 50 // Debounce delay in milliseconds
+#define debounce 50  // Debounce delay in milliseconds
 
 #define LCD_COLS 20
 #define LCD_ROWS 4
@@ -34,55 +36,55 @@
 // Format: `{definition}{PROTOCOL_SEP}`
 
 // The commands that can be sent to the MCU.
-#define PCMD_GET_PROTOCOL_VERSION "pro" // Get the MCU's protocol version
-#define PCMD_STANDBY "stb"              // Put the MCU in standby mode
-#define SHOW_STATISTICS = "sts"         // Show the statistics of the system
+#define PCMD_GET_PROTOCOL_VERSION "pro"  // Get the MCU's protocol version
+#define PCMD_STANDBY "stb"               // Put the MCU in standby mode
+#define PCMD_SHOW_STATISTICS "sts"       // Show the statistics of the system
 
-#define PCMD_GATE1_STATUS "g1s" // Get gate 1 status
-#define PCMD_GATE2_STATUS "g2s" // Get gate 2 status
-#define PCMD_GATE3_STATUS "g3s" // Get gate 3 status
-#define PCMD_GATE4_STATUS "g4s" // Get gate 4 status
-#define PCMD_COUNT_GATE5 "ct5"  // Count the object in bucket 5
+#define PCMD_GATE1_STATUS "g1s"  // Get gate 1 status
+#define PCMD_GATE2_STATUS "g2s"  // Get gate 2 status
+#define PCMD_GATE3_STATUS "g3s"  // Get gate 3 status
+#define PCMD_GATE4_STATUS "g4s"  // Get gate 4 status
+#define PCMD_COUNT_GATE5 "ct5"   // Count the object in bucket 5
 
-#define PCMD_PLATFORM_STATUS "ps" // Get the platform status
-#define PCMD_PLATFORM_OPEN "pso"  // Open platform
-#define PCMD_PLATFORM_CLOSE "psc" // Close platform
+#define PCMD_PLATFORM_STATUS "ps"  // Get the platform status
+#define PCMD_PLATFORM_OPEN "pso"   // Open platform
+#define PCMD_PLATFORM_CLOSE "psc"  // Close platform
 
-#define PCMD_GATE1_OPEN "g1o"  // Open gate 1
-#define PCMD_GATE2_OPEN "g2o"  // Open gate 2
-#define PCMD_GATE3_OPEN "g3o"  // Open gate 3
-#define PCMD_GATE4_OPEN "g4o"  // Open gate 4
-#define PCMD_GATE1_CLOSE "g1c" // Close gate 1
-#define PCMD_GATE2_CLOSE "g2c" // Close gate 2
-#define PCMD_GATE3_CLOSE "g3c" // Close gate 3
-#define PCMD_GATE4_CLOSE "g4c" // Close gate 4
+#define PCMD_GATE1_OPEN "g1o"   // Open gate 1
+#define PCMD_GATE2_OPEN "g2o"   // Open gate 2
+#define PCMD_GATE3_OPEN "g3o"   // Open gate 3
+#define PCMD_GATE4_OPEN "g4o"   // Open gate 4
+#define PCMD_GATE1_CLOSE "g1c"  // Close gate 1
+#define PCMD_GATE2_CLOSE "g2c"  // Close gate 2
+#define PCMD_GATE3_CLOSE "g3c"  // Close gate 3
+#define PCMD_GATE4_CLOSE "g4c"  // Close gate 4
 
-#define PCMD_IR_STATUS "irs" // Get IR sensor status
-#define PCMD_IR_ACK_1 "ir1"  // Acknowledge IR sensor 1 detection
-#define PCMD_IR_ACK_2 "ir2"  // Acknowledge IR sensor 2 detection
-#define PCMD_IR_ACK_3 "ir3"  // Acknowledge IR sensor 3 detection
-#define PCMD_IR_ACK_4 "ir4"  // Acknowledge IR sensor 4 detection
-#define PCMD_IR_ACK_5 "ir5"  // Acknowledge IR sensor 5 detection
+#define PCMD_IR_STATUS "irs"  // Get IR sensor status
+#define PCMD_IR_ACK_1 "ir1"   // Acknowledge IR sensor 1 detection
+#define PCMD_IR_ACK_2 "ir2"   // Acknowledge IR sensor 2 detection
+#define PCMD_IR_ACK_3 "ir3"   // Acknowledge IR sensor 3 detection
+#define PCMD_IR_ACK_4 "ir4"   // Acknowledge IR sensor 4 detection
+#define PCMD_IR_ACK_5 "ir5"   // Acknowledge IR sensor 5 detection
 
-#define PCMD_ERR_LED_STATUS "els" // Get error LED status
-#define PCMD_ERR_LED_ON "elh"     // Turn on error LED
-#define PCMD_ERR_LED_OFF "ell"    // Turn off error LED
+#define PCMD_ERR_LED_STATUS "els"  // Get error LED status
+#define PCMD_ERR_LED_ON "elh"      // Turn on error LED
+#define PCMD_ERR_LED_OFF "ell"     // Turn off error LED
 
 // The responses that can be received from the MCU.
-#define PRES_READY "RDY"            // The MCU is ready
-#define PRES_SUCCESS "OK"           // The command was successful
-#define PRES_PLATFORM_SUCCESS "OKP" // The platform command was successful
-#define PRES_FAILURE "KO"           // The command failed
-#define PRES_PVER_PREFIX "PV:"      // The protocol version prefix
-#define PRES_GATE_OPEN "GO"         // The gate is open
-#define PRES_GATE_CLOSED "GC"       // The gate is closed
-#define PRES_COUNT_GATE5 "CT"       // The count object 5
-#define PRES_PLATFORM_OPEN "PO"     // The platform is open
-#define PRES_PLATFORM_CLOSED "PC"   // The platform is closed
-#define PRES_ERR_LED_ON "ELH"       // The error LED is on
-#define PRES_ERR_LED_OFF "ELL"      // The error LED is off
+#define PRES_READY "RDY"             // The MCU is ready
+#define PRES_SUCCESS "OK"            // The command was successful
+#define PRES_PLATFORM_SUCCESS "OKP"  // The platform command was successful
+#define PRES_FAILURE "KO"            // The command failed
+#define PRES_PVER_PREFIX "PV:"       // The protocol version prefix
+#define PRES_GATE_OPEN "GO"          // The gate is open
+#define PRES_GATE_CLOSED "GC"        // The gate is closed
+#define PRES_COUNT_GATE5 "CT"        // The count object 5
+#define PRES_PLATFORM_OPEN "PO"      // The platform is open
+#define PRES_PLATFORM_CLOSED "PC"    // The platform is closed
+#define PRES_ERR_LED_ON "ELH"        // The error LED is on
+#define PRES_ERR_LED_OFF "ELL"       // The error LED is off
 
-#define PRES_IR_STATUS "IS:" // IR sensor status prefix
+#define PRES_IR_STATUS "IS:"  // IR sensor status prefix
 
 // Track the state of servos
 bool gate1_open = false;
@@ -122,49 +124,40 @@ Servo platform;
 LiquidCrystal_I2C lcd(0x27, LCD_COLS, LCD_ROWS);
 
 String encodeMessage(String message) { return message + PROTOCOL_SEP; }
-String decodeMessage(String message)
-{
+String decodeMessage(String message) {
   return message.substring(0, message.indexOf(PROTOCOL_SEP));
 }
 
 void updateLCDContents(bool clear, String line1, String line2, String line3,
-                       String line4)
-{
-  if (clear)
-    lcd.clear();
+                       String line4) {
+  if (clear) lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(line1);
-  for (int i = line1.length(); i < LCD_COLS; i++)
-  {
+  for (int i = line1.length(); i < LCD_COLS; i++) {
     lcd.print(" ");
   }
   lcd.setCursor(0, 1);
   lcd.print(line2);
-  for (int i = line2.length(); i < LCD_COLS; i++)
-  {
+  for (int i = line2.length(); i < LCD_COLS; i++) {
     lcd.print(" ");
   }
   lcd.setCursor(0, 2);
   lcd.print(line3);
-  for (int i = line3.length(); i < LCD_COLS; i++)
-  {
+  for (int i = line3.length(); i < LCD_COLS; i++) {
     lcd.print(" ");
   }
   lcd.setCursor(0, 3);
   lcd.print(line4);
-  for (int i = line4.length(); i < LCD_COLS; i++)
-  {
+  for (int i = line4.length(); i < LCD_COLS; i++) {
     lcd.print(" ");
   }
 }
 
-void showStandbyScreen()
-{
+void showStandbyScreen() {
   updateLCDContents(true, PROGRAM_NAME, PROTOCOL_VERSION, "Waiting...", "");
 }
 
-void showBucketStatistics()
-{
+void showBucketStatistics() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Canned Goods: " + String(bucket1_count));
@@ -177,34 +170,32 @@ void showBucketStatistics()
 }
 
 void checkProximitySensor(int sensorPin, unsigned long &lastDebounceTime,
-                          bool &ir_detected)
-{
+                          bool &ir_detected) {
   int sensorValue = digitalRead(sensorPin);
-  if (sensorValue ==
-      LOW)
-  { // Change HIGH to LOW to check for absence of object
+  // Change HIGH to LOW to check for absence of object
+  if (sensorValue == LOW) {
     unsigned long currentTime = millis();
-    if ((currentTime - lastDebounceTime) > debounce)
-    {
+    if ((currentTime - lastDebounceTime) > debounce) {
       ir_detected = true;
       lastDebounceTime = currentTime;
     }
   }
 }
 
-void setup()
-{
+void setup() {
   // Initialize the servos
   gate1.attach(pin_servo_gate1);
   gate2.attach(pin_servo_gate2);
   gate3.attach(pin_servo_gate3);
   gate4.attach(pin_servo_gate4);
+  platform.attach(pin_servo_platform);
 
   // Start with all servos closed
   gate1.write(servo_angle_close);
   gate2.write(servo_angle_close);
   gate3.write(servo_angle_close);
   gate4.write(servo_angle_close);
+  platform.write(servo_angle_close);
 
   // Initialize the IR sensors
   pinMode(pin_ir_bucket1, INPUT);
@@ -221,134 +212,95 @@ void setup()
   showStandbyScreen();
 
   Serial.begin(BAUDRATE);
-  Serial.print(encodeMessage(PRES_READY)); // signal to SBC that MCU is ready
+  Serial.print(encodeMessage(PRES_READY));  // signal to SBC that MCU is ready
 }
 
-void loop()
-{
-  if (Serial.available() > 0)
-  {
+void loop() {
+  if (Serial.available() > 0) {
     String command = decodeMessage(Serial.readStringUntil(PROTOCOL_SEP));
-    if (command.equalsIgnoreCase(PCMD_GET_PROTOCOL_VERSION))
-    {
+    if (command.equalsIgnoreCase(PCMD_GET_PROTOCOL_VERSION)) {
       Serial.print(
           encodeMessage(String(PRES_PVER_PREFIX) + String(PROTOCOL_VERSION)));
-    }
-    else if (command.equalsIgnoreCase(PCMD_STANDBY))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_STANDBY)) {
       showStandbyScreen();
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_SHOW_STATISTICS))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_SHOW_STATISTICS)) {
       showBucketStatistics();
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_ERR_LED_STATUS))
-    {
-      Serial.print(encodeMessage(is_err_led_on ? PRES_ERR_LED_ON : PRES_ERR_LED_OFF));
-    }
-    else if (command.equalsIgnoreCase(PCMD_ERR_LED_ON))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_ERR_LED_STATUS)) {
+      Serial.print(
+          encodeMessage(is_err_led_on ? PRES_ERR_LED_ON : PRES_ERR_LED_OFF));
+    } else if (command.equalsIgnoreCase(PCMD_ERR_LED_ON)) {
       digitalWrite(pin_error_led, HIGH);
+      is_err_led_on = true;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_ERR_LED_OFF))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_ERR_LED_OFF)) {
       digitalWrite(pin_error_led, LOW);
+      is_err_led_on = false;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE1_STATUS))
-    {
-      Serial.print(encodeMessage(gate1_open ? PRES_GATE_OPEN : PRES_GATE_CLOSED));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE2_STATUS))
-    {
-      Serial.print(encodeMessage(gate2_open ? PRES_GATE_OPEN : PRES_GATE_CLOSED));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE3_STATUS))
-    {
-      Serial.print(encodeMessage(gate3_open ? PRES_GATE_OPEN : PRES_GATE_CLOSED));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE4_STATUS))
-    {
-      Serial.print(encodeMessage(gate4_open ? PRES_GATE_OPEN : PRES_GATE_CLOSED));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE1_OPEN))
-    {
-      gate1.write(servo_angle_open);
+    } else if (command.equalsIgnoreCase(PCMD_GATE1_STATUS)) {
+      Serial.print(
+          encodeMessage(gate1_open ? PRES_GATE_OPEN : PRES_GATE_CLOSED));
+    } else if (command.equalsIgnoreCase(PCMD_GATE2_STATUS)) {
+      Serial.print(
+          encodeMessage(gate2_open ? PRES_GATE_OPEN : PRES_GATE_CLOSED));
+    } else if (command.equalsIgnoreCase(PCMD_GATE3_STATUS)) {
+      Serial.print(
+          encodeMessage(gate3_open ? PRES_GATE_OPEN : PRES_GATE_CLOSED));
+    } else if (command.equalsIgnoreCase(PCMD_GATE4_STATUS)) {
+      Serial.print(
+          encodeMessage(gate4_open ? PRES_GATE_OPEN : PRES_GATE_CLOSED));
+    } else if (command.equalsIgnoreCase(PCMD_GATE1_OPEN)) {
+      gate1.write(servo_angle_open12);
       gate1_open = true;
       bucket1_count++;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE2_OPEN))
-    {
-      gate2.write(servo_angle_open);
+    } else if (command.equalsIgnoreCase(PCMD_GATE2_OPEN)) {
+      gate2.write(servo_angle_open12);
       gate2_open = true;
       bucket2_count++;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE3_OPEN))
-    {
-      gate3.write(servo_angle_open);
+    } else if (command.equalsIgnoreCase(PCMD_GATE3_OPEN)) {
+      gate3.write(servo_angle_open34);
       gate3_open = true;
       bucket3_count++;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE4_OPEN))
-    {
-      gate4.write(servo_angle_open);
+    } else if (command.equalsIgnoreCase(PCMD_GATE4_OPEN)) {
+      gate4.write(servo_angle_open34);
       gate4_open = true;
       bucket4_count++;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE1_CLOSE))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_GATE1_CLOSE)) {
       gate1.write(servo_angle_close);
       gate1_open = false;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE2_CLOSE))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_GATE2_CLOSE)) {
       gate2.write(servo_angle_close);
       gate2_open = false;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE3_CLOSE))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_GATE3_CLOSE)) {
       gate3.write(servo_angle_close);
       gate3_open = false;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_GATE4_CLOSE))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_GATE4_CLOSE)) {
       gate4.write(servo_angle_close);
       gate4_open = false;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_COUNT_GATE5))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_COUNT_GATE5)) {
       bucket5_count++;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_PLATFORM_STATUS))
-    {
-      Serial.print(encodeMessage(platform_open ? PRES_PLATFORM_OPEN : PRES_PLATFORM_CLOSED));
-    }
-    else if (command.equalsIgnoreCase(PCMD_PLATFORM_OPEN))
-    {
-      platform.write(servo_angle_open);
-      gate4_open = true;
+    } else if (command.equalsIgnoreCase(PCMD_PLATFORM_STATUS)) {
+      Serial.print(encodeMessage(platform_open ? PRES_PLATFORM_OPEN
+                                               : PRES_PLATFORM_CLOSED));
+    } else if (command.equalsIgnoreCase(PCMD_PLATFORM_OPEN)) {
+      platform.write(servo_angle_open_platform);
+      platform_open = true;
       Serial.print(encodeMessage(PRES_PLATFORM_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_PLATFORM_CLOSE))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_PLATFORM_CLOSE)) {
       platform.write(servo_angle_close);
       platform_open = false;
       Serial.print(encodeMessage(PRES_PLATFORM_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_IR_STATUS))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_IR_STATUS)) {
       String ir_status = PRES_IR_STATUS;
       ir_status += ir_detected_bucket1 ? "1" : "0";
       ir_status += ir_detected_bucket2 ? "1" : "0";
@@ -356,34 +308,22 @@ void loop()
       ir_status += ir_detected_bucket4 ? "1" : "0";
       ir_status += ir_detected_bucket5 ? "1" : "0";
       Serial.print(encodeMessage(ir_status));
-    }
-    else if (command.equalsIgnoreCase(PCMD_IR_ACK_1))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_IR_ACK_1)) {
       ir_detected_bucket1 = false;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_IR_ACK_2))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_IR_ACK_2)) {
       ir_detected_bucket2 = false;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_IR_ACK_3))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_IR_ACK_3)) {
       ir_detected_bucket3 = false;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_IR_ACK_4))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_IR_ACK_4)) {
       ir_detected_bucket4 = false;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else if (command.equalsIgnoreCase(PCMD_IR_ACK_5))
-    {
+    } else if (command.equalsIgnoreCase(PCMD_IR_ACK_5)) {
       ir_detected_bucket5 = false;
       Serial.print(encodeMessage(PRES_SUCCESS));
-    }
-    else
-    {
+    } else {
       Serial.print(encodeMessage(PRES_FAILURE));
     }
 
